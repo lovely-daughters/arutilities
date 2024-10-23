1cd04e50ad04b62765b1afcaccce172df4a81b47
0eti <samuelzhang012@gmail.com>
2024-10-22 20:21:16 -0700

    works for image and video if there's only one in post. now to download all relevant media.

--- apps/extension/src/content-scripts/twitter-2.ts
+++ apps/extension/src/content-scripts/twitter-2.ts
@@ -192,8 +192,10 @@ document.addEventListener("mousedown", (event: any) => {
   console.log(tweet);
   if (tweet) {
     const tweet_url = findTweetUrl(tweet);
-    const tweet_id = tweet_url?.split("/status/").pop();
-    console.log(tweet_id);
+    const splits = tweet_url!.split("/");
+    const tweet_author = splits![1];
+    const tweet_id = splits![3];
+    console.log(tweet_author, tweet_id);
 
     fetch(
       `https://x.com/i/api/graphql/nBS-WpgA6ZG0CyNHD517JQ/TweetDetail?variables=%7B%22focalTweetId%22%3A%22${tweet_id}%22%2C%22with_rux_injections%22%3Afalse%2C%22rankingMode%22%3A%22Relevance%22%2C%22includePromotedContent%22%3Atrue%2C%22withCommunity%22%3Atrue%2C%22withQuickPromoteEligibilityTweetFields%22%3Atrue%2C%22withBirdwatchNotes%22%3Atrue%2C%22withVoice%22%3Atrue%7D&features=%7B%22rweb_tipjar_consumption_enabled%22%3Atrue%2C%22responsive_web_graphql_exclude_directive_enabled%22%3Atrue%2C%22verified_phone_label_enabled%22%3Afalse%2C%22creator_subscriptions_tweet_preview_api_enabled%22%3Atrue%2C%22responsive_web_graphql_timeline_navigation_enabled%22%3Atrue%2C%22responsive_web_graphql_skip_user_profile_image_extensions_enabled%22%3Afalse%2C%22communities_web_enable_tweet_community_results_fetch%22%3Atrue%2C%22c9s_tweet_anatomy_moderator_badge_enabled%22%3Atrue%2C%22articles_preview_enabled%22%3Atrue%2C%22responsive_web_edit_tweet_api_enabled%22%3Atrue%2C%22graphql_is_translatable_rweb_tweet_is_translatable_enabled%22%3Atrue%2C%22view_counts_everywhere_api_enabled%22%3Atrue%2C%22longform_notetweets_consumption_enabled%22%3Atrue%2C%22responsive_web_twitter_article_tweet_consumption_enabled%22%3Atrue%2C%22tweet_awards_web_tipping_enabled%22%3Afalse%2C%22creator_subscriptions_quote_tweet_preview_enabled%22%3Afalse%2C%22freedom_of_speech_not_reach_fetch_enabled%22%3Atrue%2C%22standardized_nudges_misinfo%22%3Atrue%2C%22tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled%22%3Atrue%2C%22rweb_video_timestamps_enabled%22%3Atrue%2C%22longform_notetweets_rich_text_read_enabled%22%3Atrue%2C%22longform_notetweets_inline_media_enabled%22%3Atrue%2C%22responsive_web_enhance_cards_enabled%22%3Afalse%7D&fieldToggles=%7B%22withArticleRichContentState%22%3Atrue%2C%22withArticlePlainText%22%3Afalse%2C%22withGrokAnalyze%22%3Afalse%2C%22withDisallowedReplyControls%22%3Afalse%7D`,
@@ -273,24 +275,31 @@ document.addEventListener("mousedown", (event: any) => {
           const large_photo_url =
             photo_url.split(".jpg")[0] + "?format=jpg&name=large";
 
-          fetch(large_photo_url)
-            .then((response) => response.blob())
-            .then((blob) => {
-              const blobUrl = URL.createObjectURL(blob);
-              // Create an anchor element and trigger a download
-              const a = document.createElement("a");
-              a.href = blobUrl;
-              if (type == "animated_gif" || type == "video") {
-                a.download = `${tweet_id}.mp4`;
-              } else if (type == "photo") {
-                a.download = `${tweet_id}.jpg`;
-              }
-              document.body.appendChild(a);
-              a.click();
-              // Clean up
-              a.remove();
-              URL.revokeObjectURL(blobUrl);
-            });
+          sendDownloadMessage(
+            tweet_author,
+            tweet_id,
+            1,
+            large_photo_url,
+            "jpg"
+          );
+          // fetch(large_photo_url)
+          //   .then((response) => response.blob())
+          //   .then((blob) => {
+          //     const blobUrl = URL.createObjectURL(blob);
+          //     // Create an anchor element and trigger a download
+          //     const a = document.createElement("a");
+          //     a.href = blobUrl;
+          //     if (type == "animated_gif" || type == "video") {
+          //       a.download = `${tweet_id}.mp4`;
+          //     } else if (type == "photo") {
+          //       a.download = `${tweet_id}.jpg`;
+          //     }
+          //     document.body.appendChild(a);
+          //     a.click();
+          //     // Clean up
+          //     a.remove();
+          //     URL.revokeObjectURL(blobUrl);
+          //   });
         } else if (type === "video" || type === "animated_gif") {
           const video_variants = media.video_info.variants;
           // @ts-ignore
@@ -298,48 +307,55 @@ document.addEventListener("mousedown", (event: any) => {
             prev.bitrate > current.bitrate ? prev : current
           );
           console.log(highest_bitrate.url);
+          sendDownloadMessage(
+            tweet_author,
+            tweet_id,
+            1,
+            highest_bitrate.url,
+            "jpg"
+          );
           // fetch video and save it to downloads
-          fetch(highest_bitrate.url)
-            .then((response: Response) => {
-              const reader = response.body!.getReader();
-              const chunks: any[] = [];
+          // fetch(highest_bitrate.url)
+          //   .then((response: Response) => {
+          //     const reader = response.body!.getReader();
+          //     const chunks: any[] = [];
 
-              return reader
-                .read()
-                .then(function processChunk({ done, value }): any {
-                  if (done) {
-                    // Create a blob from the collected chunks
-                    const blob = new Blob(chunks, { type: "video/mp4" });
-                    const blobUrl = URL.createObjectURL(blob);
+          //     return reader
+          //       .read()
+          //       .then(function processChunk({ done, value }): any {
+          //         if (done) {
+          //           // Create a blob from the collected chunks
+          //           const blob = new Blob(chunks, { type: "video/mp4" });
+          //           const blobUrl = URL.createObjectURL(blob);
 
-                    // Create an anchor element and trigger a download
-                    const a = document.createElement("a");
-                    a.href = blobUrl;
+          //           // Create an anchor element and trigger a download
+          //           const a = document.createElement("a");
+          //           a.href = blobUrl;
 
-                    if (type == "animated_gif" || type == "video") {
-                      a.download = `${tweet_id}.mp4`;
-                    } else if (type == "photo") {
-                      a.download = `${tweet_id}.mp4`;
-                    }
-                    document.body.appendChild(a);
-                    a.click();
+          //           if (type == "animated_gif" || type == "video") {
+          //             a.download = `${tweet_id}.mp4`;
+          //           } else if (type == "photo") {
+          //             a.download = `${tweet_id}.mp4`;
+          //           }
+          //           document.body.appendChild(a);
+          //           a.click();
 
-                    // Clean up
-                    a.remove();
-                    URL.revokeObjectURL(blobUrl);
-                    return;
-                  }
+          //           // Clean up
+          //           a.remove();
+          //           URL.revokeObjectURL(blobUrl);
+          //           return;
+          //         }
 
-                  // Append chunk to chunks array
-                  chunks.push(value);
+          //         // Append chunk to chunks array
+          //         chunks.push(value);
 
-                  // Continue reading the stream
-                  return reader.read().then(processChunk);
-                });
-            })
-            .catch((err) => {
-              console.error("Failed to download video:", err);
-            });
+          //         // Continue reading the stream
+          //         return reader.read().then(processChunk);
+          //       });
+          //   })
+          //   .catch((err) => {
+          //     console.error("Failed to download video:", err);
+          //   });
         }
       })
       .catch((err) => {
